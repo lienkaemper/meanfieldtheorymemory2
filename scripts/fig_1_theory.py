ac32ba03ff1f66cac43b87b0fb8fad991dfb4c7a
lienkaemper <caitlienk@gmail.com>
2024-01-31 18:34:13 -0500

    working with only population level model

--- scripts/fig_1_theory.py
+++ scripts/fig_1_theory.py
@@ -9,7 +9,7 @@ import os
 import sys
 
 
-from src.theory import y_pred_from_full_connectivity, y_corrected_quad, y_0_quad, covariance_full, loop_correction
+from src.theory import y_pred_from_full_connectivity, y_corrected_quad, y_0_quad, covariance_full, loop_correction, cor_pred
 from src.correlation_functions import rate, sum_by_region, cov_to_cor
 from src.generate_connectivity import excitatory_only, gen_adjacency, hippo_weights, macro_weights
 
@@ -34,6 +34,7 @@ with open(dirname + "/param_dict.pkl", "rb") as file:
 
 
 N = param_dict["N"]
+Ns = np.array([len(R) for R in index_dict.values()])
 h_min = param_dict["h_min"]
 h_max = param_dict["h_max"]
 
@@ -50,17 +51,13 @@ for i, key in enumerate(index_dict.keys()):
 n_h = 10
 h_range = np.linspace(h_min, h_max, n_h)
 
-region_list = ['' for i in range(N)]
-for key in index_dict:
-    start = min(index_dict[key])
-    end = max(index_dict[key])
-    region_list[start:end+1] = (end+1-start)*[key]
+region_list = list(index_dict.keys())
 
 region_i_list = []
 region_j_list = []
-for i in range(N):
-    for j in range(N):
-        if i < j:
+for i in range(6):
+    for j in range(6):
+        if i <= j:
             region_i_list.append(region_list[i])
             region_j_list.append(region_list[j])
         
@@ -69,8 +66,8 @@ region_list = len(h_range) * region_list
 region_i_list = len(h_range) * region_i_list
 region_j_list = len(h_range) * region_j_list
 
-h_list = [h for h in h_range for i in range(N)]
-h_list_cor = [h for h in h_range for i in range(int(N*(N-1)/2))]
+h_list = [h for h in h_range for i in range(6)]
+h_list_cor = [h for h in h_range for i in range(int(6*(6-1)/2+6))]
 
 
 
@@ -82,31 +79,27 @@ for h in h_range:
     J =  hippo_weights(index_dict, A, h,h, g, J0, i_plast = 1, g_ii = g_ii)
     #J_small = macro_weights(J = J0, h3 = h, h1 = h, g = g, g_ii = g_ii)
     J_small = sum_by_region(J, index_dict=index_dict)
-    y_q = y_0_quad(J,b)
+    #y_q = y_0_quad(J,b)
     
-
-
     y_q_red = y_0_quad(J_small, b_small)
-    correction_small = loop_correction(J_small,  y_q_red, b_small)
-    y_q_red_long = np.zeros(N)
-    for i, key in enumerate(index_dict):
-        y_q_red_long[index_dict[key]] = np.real(correction_small[i]) +  y_q_red[i]
-        y_q[index_dict[key]] =  y_q_red[i]
+    correction = np.real(loop_correction(J_small,  y_q_red, b_small))
+    y_corrected = y_q_red + correction 
         
-    pred_rates.extend(y_q)
-    reduced_rates.extend(y_q_red_long)
+    pred_rates.extend(y_q_red)
+    reduced_rates.extend(y_corrected)
 
 
-    J_lin =J* (2*(J@y_q_red_long + b))[...,None]
-    pred_covs = covariance_full(J_lin, y_q_red_long)
-    pred_cors_mat = cov_to_cor(pred_covs)
-    pred_cors.extend( pred_cors_mat[np.triu_indices(N,1)])
+    J_lin =J_small* (2*(J_small@y_corrected + b_small))[...,None]
+    pred_cors_mat = cor_pred(J_lin, Ns, y_corrected)
+    pred_cors.extend( pred_cors_mat[np.triu_indices(6)])
 
 
 print(len(reduced_rates))
 print(len(pred_rates))
+
 rate_df = pd.DataFrame({"pred_rate":pred_rates, "h" : h_list, "region": region_list ,  "reduced_rate": reduced_rates})
 cor_df = pd.DataFrame({"pred_cor": pred_cors, "region_i" : region_i_list, "region_j": region_j_list, "h": h_list_cor})
-
+print(rate_df)
+print(cor_df)
 rate_df.to_csv("../results/fig_1_data/pred_rates.csv")
 cor_df.to_csv("../results/fig_1_data/pred_cors.csv")
